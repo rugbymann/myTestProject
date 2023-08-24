6d1dfa11e51a12ecd6fdd45192ef62483f89c197
Anar Aliyev <rugbisto@gmail.com>
2023-08-24 08:52:40 +0200

    add test lifetwig

--- src/test/java/org/ait/qa2526/LifeTwigTest.java
+++ src/test/java/org/ait/qa2526/LifeTwigTest.java
@@ -19,7 +19,7 @@ public class LifeTwigTest {
     }
     @Test
     public void LifeTwigTest(){
-        System.out.println("it's work");
+        System.out.println("site opened");
     }
     @AfterMethod
     public void tearDown(){
